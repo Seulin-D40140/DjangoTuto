079bcdf2e5d4d0a55ca81c99461e992eb0872a0e
Donovan Seulin <nel640@hotmail.fr>
2025-02-17 14:02:13 +0100

    exos 4 , 4.1 , 4.2 , 4.3 , admin.model

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from .models import Choice , Question
 
-from .models import Question
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'pub_date', 'was_published_recently')
+    list_filter = ('pub_date', 'id')
+    ordering = ('-pub_date', '-id')
+    search_fields = ('question_text',)
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'choice_text', 'votes', 'question_id')
+    list_filter = ('votes', 'id')
+    ordering = ('-votes', '-id')
+    search_fields = ('choice_text',)
+
+admin.site.register(Choice , ChoiceAdmin)
